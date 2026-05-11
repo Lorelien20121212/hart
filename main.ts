e0bb916ae9b9bfa8ebfacbad33719dcb000c0f18
Lorelien20121212 <lorelienvanginneken@gmail.com>
2026-05-11 15:53:25 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -6,4 +6,13 @@ basic.forever(function () {
         . # # # .
         . . # . .
         `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
 })
